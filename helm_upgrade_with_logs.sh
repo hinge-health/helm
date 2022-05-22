ba666d8db5689714f4f9b98699382b7eeedca19a
aaron sharpe <aaron.sharpe@hingehealth.com>
2022-05-21 21:55:24 -0700

    more test garbage

--- helm_upgrade_with_logs.sh
+++ helm_upgrade_with_logs.sh
@@ -10,88 +10,6 @@ if [[ -n "${DOTFILES_DEBUG:-}" ]]; then
   set -x
 fi
 
-show_help() {
-cat << EOF
-Usage: $(basename "$0") <options>
-    -h, --help               Display help
-    -v, --version            The chart-releaser version to use (default: $DEFAULT_CHART_RELEASER_VERSION)"
-        --config             The path to the chart-releaser config file
-    -d, --charts-dir         The charts directory (default: charts)
-    -u, --charts-repo-url    The GitHub Pages URL to the charts repo (default: https://<release>.github.io/<repo>)
-    -o, --release              The repo release
-    -r, --repo               The repo name
-    -c, --chartmuseum_url    The URL of the Chartmuseum Instance e.g. chartmuseum.example.com
-EOF
-}
-
-parse_command_line() {
-    while :; do
-        case "${1:-}" in
-            -h|--help)
-                show_help
-                exit
-                ;;
-            -r|release)
-                if [[ -n "${2:-}" ]]; then
-                    config="$2"
-                    shift
-                else
-                    echo "ERROR: '--release' cannot be empty." >&2
-                    show_help
-                    exit 1
-                fi
-                ;;
-            -v|--version)
-                if [[ -n "${2:-}" ]]; then
-                    version="$2"
-                    shift
-                else
-                    echo "ERROR: '-v|--version' cannot be empty." >&2
-                    show_help
-                    exit 1
-                fi
-                ;;
-            --n|namespace)
-                if [[ -n "${2:-}" ]]; then
-                    charts_dir="$2"
-                    shift
-                else
-                    echo "ERROR: '-n|--namespace' cannot be empty." >&2
-                    show_help
-                    exit 1
-                fi
-                ;;
-            *)
-                break
-                ;;
-        esac
-
-        shift
-    done
-
-    if [[ -z "$release" ]]; then
-        echo "ERROR: '-r|--release' is required." >&2
-        show_help
-        exit 1
-    fi
-
-    if [[ -z "$repo" ]]; then
-        echo "ERROR: '-v|--version' is required." >&2
-        show_help
-        exit 1
-    fi
-
-    if [[ -z "$namespace" ]]; then
-        echo "ERROR: '-n|--namespace' is required." >&2
-        show_help
-        exit 1
-    fi
-
-    if [[ -z "$charts_repo_url" ]]; then
-        charts_repo_url="https://$release.github.io/$repo"
-    fi
-}
-
 function ensure_path_entry() {
   local entries=("$@")
 
@@ -291,31 +209,11 @@ function get_namespace() {
   done
 }
 
-function get_image_tag() {
-  for arg in "$@"; do
-    if [[ "${arg}" = "--namespace"* ]]; then
-      namespace=`echo $arg | cut -d"=" -f 2`
-      echo "${namespace}"
-      return
-    fi
-  done
-}
 
-function main() {
-    local version=
-    local config=
-    local release=
-
-    parse_command_line "$@"
-    : "${GITHUB_TOKEN:?Environment variable GITHUB_TOKEN must be set}"
-
-    c helm upgrade "${release}" hinge-health/hinge-service --install
-}
 release="$(get_first_non_option "$@")"
 namespace="$(get_namespace "$@")"
-version=${IMAGE_TAG}
-echo "app version is ${IMAGE_TAG}"
-env
+version=${IMAGE_TAG} # should be in the environment in our github workflow
+
 c helm3 upgrade "$@" &
 pid="$!"
 
