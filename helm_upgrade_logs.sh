8e12eb7ee859c6cd7072e2b8db912df7bbc1de13
aaron sharpe <aaron.sharpe@hingehealth.com>
2022-05-21 21:24:25 -0700

    debug too

--- helm_upgrade_logs.sh
+++ helm_upgrade_logs.sh
@@ -312,7 +312,7 @@ function main() {
 release="$(get_first_non_option "$@")"
 namespace="$(get_namespace "$@")"
 env
-c helm3 upgrade "$@" --timeout 20s &
+c helm3 upgrade "$@" --timeout 20s --debug &
 pid="$!"
 
 watch_pods "${release}" "${namespace}" &
